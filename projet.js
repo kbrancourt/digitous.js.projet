0d3609a8486459870d0dde840b21219f585998f2
BeressE <el.beressa@gmail.com>
2021-01-07 02:22:02 +0100

    MoveBackward et pistes pour les limites

--- projet.js
+++ projet.js
@@ -1,7 +1,7 @@
 //étape 1
 
 var grid = [
-	[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+	[" N", " ", " ", " ", " ", " ", " ", " ", " ", " "],
 	[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
 	[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
 	[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
@@ -13,31 +13,36 @@ var grid = [
 	[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 ];
 
-
 var rover = {
-	direction : "N",
-	positionX : 0,
-	positionY : 0,
+	direction: "N",
+	positionX: 0,
+	positionY: 0,
+	/* 
+	travelLog = [], 
+	*/
 }
 
-function turnLeft(rover){
-	 
+// Voir si nous ne pouvons pas avoir une limite de grille ?
+// peut être avec une condition comme cela rover.poisitonX < 0 || rover.poisitonX > 10/9 ? Une condition dans une condition ?
 
-	if 	(rover.direction === "N"){
+function turnLeft(rover) {
+
+
+	if (rover.direction === "N") {
 		rover.direction = "W";
 		console.log(`Vous tounez à gauche ${rover.direction}`)
 
-	} else if (rover.direction === "W"){
+	} else if (rover.direction === "W") {
 		rover.direction = "S";
 		console.log(`Vous tounez à gauche ${rover.direction}`)
 
 
-	} else if (rover.direction === "S"){
+	} else if (rover.direction === "S") {
 		rover.direction = "E";
 		console.log(`Vous tounez à gauche ${rover.direction}`)
 
 
-	} else if (rover.direction === "E"){
+	} else if (rover.direction === "E") {
 		rover.direction = "N";
 		console.log(`Vous tounez à gauche ${rover.direction}`)
 
@@ -47,22 +52,22 @@ function turnLeft(rover){
 	}
 }
 
-function turnRight(rover){
-	if 	(rover.direction === "N"){
+function turnRight(rover) {
+	if (rover.direction === "N") {
 		rover.direction = "E";
 		console.log(`Vous tounez à gauche ${rover.direction}`)
 
-	} else if (rover.direction === "E"){
+	} else if (rover.direction === "E") {
 		rover.direction = "S";
 		console.log(`Vous tounez à gauche ${rover.direction}`)
 
 
-	} else if (rover.direction === "S"){
+	} else if (rover.direction === "S") {
 		rover.direction = "W";
 		console.log(`Vous tounez à gauche ${rover.direction}`)
 
 
-	} else if (rover.direction === "W"){
+	} else if (rover.direction === "W") {
 		rover.direction = "N";
 		console.log(`Vous tounez à gauche ${rover.direction}`)
 
@@ -73,19 +78,38 @@ function turnRight(rover){
 
 
 }
-function moveForward(rover){
-	if (rover.direction === "E"){
+function moveForward(rover) {
+	if (rover.direction === "E") {
 		rover.positionX += 1;
 		console.log(`J'avance de un ${rover.positionX}`)
-	} else if(rover.direction === "S"){
+	} else if (rover.direction === "S") {
 		rover.positionY -= 1;
 		console.log(`J'avance de un ${rover.positionY}`)
-	} else if (rover.direction === "W"){
+	} else if (rover.direction === "W") {
 		rover.positionX += 1;
 		console.log(`J'avance de un ${rover.positionX}`)
-	} else if (rover.direction === "N"){
+	} else if (rover.direction === "N") {
 		rover.positionY -= 1;
 		console.log(`J'avance de un ${rover.positionY}`)
+	}
+}
+
+
+// Ajout de la fonction moveBackward
+
+function moveBackward(rover) {
+	if (rover.direction === "N") {
+		rover.positionY += 1;
+		console.log(`Je suis la fonction "moveBackward" je recule ${rover.positionY}`)
+	} else if (rover.direction === "E") {
+		rover.positionX -= 1;
+		console.log(`Je suis la fonction "moveBackward" je recule ${rover.positionX}`)
+	} else if (rover.direction === "S") {
+		rover.positionY -= 1;
+		console.log(`Je suis la fonction "moveBackward" je recule ${rover.positionY}`)
+	} else if (rover.direction === "W") {
+		rover.positionX += 1
+		console.log(`Je suis la fonction "moveBackward" je recule ${rover.positionX}`)
 	}
 }
 
@@ -111,22 +135,34 @@ function moveForward(rover){
 }*/
 
 //Test tard le soir
-function pilotRover(string){
-	for(var i = 0; i< string.length; i++){
-		if(string === "l"){
+
+// Il faut voir comment prendre toutes les versions d'une lettre et donc avoir peut-être un toUpperCase 
+// exemple : r => R, l = L, f => F
+function pilotRover(string) {
+	var i = 0;
+	for (var i = 0; i < string.length; i++) {
+		if (string === "l") {
 			turnLeft(rover)
 			console.log(`Je suis la fonction pilotRover je tourne à gauche`)
-		} else if(string ==="r"){
+		} else if (string === "r") {
 			turnRight("r")
 			console.log(`Je suis la fonction pilotRover je tourne à droite`)
-		} else if(string === "f"){
+		} else if (string === "f") {
 			moveForward(rover)
 			console.log(`Je suis la fonction pilotRover j'avance`)
+		} else if (string === "b") {
+			moveBackward(rover)
 		}
 	}
 }
 
-pilotRover("f","r")
+
+
+pilotRover("b")
 
 console.log(rover)
 
+/*
+console.log(grid)
+
+*/
